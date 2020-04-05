8e33ac49453f61d5e071b4896628e7437294619f
meri <mdiaz>
2020-04-05 11:48:28 +0200

    transcribiendo la primera practica

---
+++ project/acorta/views.py
@@ -0,0 +1,41 @@
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import get_object_or_404, render, redirect
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import UrlsAcortada
+
+@csrf_exempt
+def index(request):
+
+        url_list = UrlsAcortada.objects.all()
+        context = {'url_list': url_list}
+        return render(request, 'acorta/index.html', context)
+
+
+
+@csrf_exempt
+def get_content(request, llave):
+    if request.method == "GET":
+        url = get_object_or_404(UrlsAcortada, id=llave).url
+        return redirect("http://" + url)
+
+    elif request.method == "POST":
+        valor = request.POST['valor']
+        if valor == "":
+            return HttpResponseNotFound("Introduce una url para acortar")
+
+        if not(valor.startswith('http://') or
+            valor.startswith('https://')):
+            requested_item = valor
+            valor = 'http://' + valor
+            else:
+            requested_item = valor[valor.index('/') + 2:]
+
+        try:
+            c = UrlsAcortada.objects.get(url=requested_item)
+        except UrlsAcortada.DoesNotExist:
+            c = UrlsAcortada(url = requested_item)
+            c.save()
+
+        context = {'urlPedida': c}
+        return render(request, 'acorta/url_pinchable.html', context)
